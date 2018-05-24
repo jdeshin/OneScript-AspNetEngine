b0f5812f4c675cb24cf57fa7f0f8f594481d19b9
yury deshin <j.deshin@hotmail.com>
2018-05-24 12:04:12 +0300

    Поправил readme DataProcessos

--- DataProcessors/DataProcessorManagerImpl.cs
+++ DataProcessors/DataProcessorManagerImpl.cs
@@ -99,12 +99,13 @@ namespace OneScript.HTTPService
             foreach (string typeInfo in appSettings.AllKeys)
             {
                 string info = typeInfo.Replace(" ", "");
-                if (info.StartsWith("attachAsDataProcessor"))
+                if (info.StartsWith("attachAsDataProcessor;"))
                 {
-                    // Запись должна быть вида <key="ИмяСборки;ИмяТипа;[ИмяОбработки]"
+                    // Запись должна быть вида <key="attachAsDataProcessor;ИмяСборки;ИмяТипа" value="[ИмяОбработки]" />
                     // Если пункта ИмяОбработки нет - получаем из атрибута класса
                     string[] dataProcessorInfo = info.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
-
+                    System.IO.TextWriter tw = System.IO.File.CreateText("c:\\1\\obr.txt");
+                    tw.WriteLine(dataProcessorInfo.Length.ToString());
                     if (dataProcessorInfo.Length !=3)
                         continue;
 
@@ -113,7 +114,11 @@ namespace OneScript.HTTPService
                     dataProcessorName = dataProcessorName.Replace(" ", "");
                     string assemblyName = dataProcessorInfo[1].Trim();
                     object instance = Activator.CreateInstance(assemblyName, typeName).Unwrap();
-
+                   
+                    tw.WriteLine(typeName);
+                    tw.Write(assemblyName);
+                    tw.Write(dataProcessorName);
+                    tw.Close();
                     if (dataProcessorName == "")
                     {
                         ContextClassAttribute attribute = instance.GetType().GetCustomAttributes(typeof(ContextClassAttribute), false).FirstOrDefault() as ContextClassAttribute;
