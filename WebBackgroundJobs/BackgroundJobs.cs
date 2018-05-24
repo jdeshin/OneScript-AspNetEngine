cabaafd3f875607bb9e333c426bfe1e7c4765258
yury deshin <j.deshin@hotmail.com>
2018-05-24 12:15:22 +0300

    Добавил загрузку WebBackgroundJobs как свойства

---
+++ WebBackgroundJobs/BackgroundJobs.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Yury Deshin 2018
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScriptEngine.HostedScript;
+
+
+namespace OneScript.HTTPService
+{
+    public class BackgroundJobs : ILibraryAsPropertiesLoader
+    {
+        public List<string> GetPropertiesNamesForInjecting(string info)
+        {
+            List<string> propNames = new List<string>();
+            propNames.Add("ФоновыеЗадания");
+            return propNames;
+        }
+
+        public void AssignPropertiesValues(HostedScriptEngine engine)
+        {
+            engine.EngineInstance.Environment.SetGlobalProperty("ФоновыеЗадания", new WebBackgroundJobsManagerImpl());
+        }
+    }
+}
