2387f252709ea7ec222f0695507e3848fb3e4e6d
yury deshin <j.deshin@hotmail.com>
2018-05-28 18:36:41 +0300

    Поправил объявление функции в интерфейсе

--- SqlDataProcessor/SqlDataProcessor.cs
+++ SqlDataProcessor/SqlDataProcessor.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SqlDataProcessor
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library;
+using ScriptEngine.HostedScript.Library.Binary;
+
+using ScriptEngine;
+using ScriptEngine.Environment;
+using ScriptEngine.HostedScript;
+
+namespace OneScript.SqlDataProcessor
 {
-    public class SqlDataProcessor
+    [ContextClass("СоединениеСУБД", "DBConnection")]
+    public class DBConnector : AutoContext<DBConnector>
     {
+        [ContextMethod("Создать", "Create")]
+        public IValue Create()
+        {
+            // Создаем объект из модуля объекта
+
+            return new OScriptSql.;
+        }
     }
 }
